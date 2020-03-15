fc6cfc6145828e763c97176deb9d687a5c51107f
DaryaDan <56804467+DaryaDan@users.noreply.github.com>
2020-03-15 23:42:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (typeof str !== "string" || str.trim().length === 0) {
+  return false;
+  }
+  while (str.length > 0) {
+  let a = str.length;
+  str = str.replace(/\(\)/g, "");
+  str = str.replace(/\{\}/g, "");
+  str = str.replace(/\|\|/g, "");
+  str = str.replace(/\[\]/g, "");
+  str = str.replace(/12/g, "");
+  str = str.replace(/34/g, "");
+  str = str.replace(/56/g, "");
+  str = str.replace(/77/g, "");
+  str = str.replace(/88/g, "");
+  if (str.length === a) {
+  return false;
+  }
+  }
+  return true;
+  }
